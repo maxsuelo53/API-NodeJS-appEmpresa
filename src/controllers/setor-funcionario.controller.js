528b264c8bb6bac5845c0ccc676ec87ab0a812c8
Maxsuel Oliveira Alves <maxsuelo53@gmail.com>
2021-07-01 16:09:28 -0300

    importando versao

--- src/controllers/setor-funcionario.controller.js
+++ src/controllers/setor-funcionario.controller.js
@@ -17,3 +17,22 @@ exports.createSetorFuncionario = async (req, res) => {
     });
   }
 };
+
+
+exports.createSetorFuncionario = async (req, res) => {
+  const { id_funcionario, id_setor} = req.body;
+  try {
+    const { rows } = await db.query(
+      "INSERT INTO setor_funcionario (id_funcionario, id_setor) VALUES ($1, $2)",
+      [id_funcionario, id_setor]
+    );
+    res.status(201).send({
+      message: "Setor preenchido added successfully!"
+    });
+  } catch (error) {
+    console.error('createSetorFuncionario', error);
+    res.status(500).send({
+      message: "Ocorreu um erro."
+    });
+  }
+};
